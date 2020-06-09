ebae7d11386c91213f5df1e5d19477e29c7f1637
anas boumediene <anexbm@gmail.com>
2020-06-09 12:29:49 +0200

    add moveBook function

--- src/books/BookShelf.js
+++ src/books/BookShelf.js
@@ -1,19 +1,33 @@
 
 import React from 'react'
 import Book from './Book';
+import * as BooksApi from '../BooksAPI';
+
 export default class BookShelf extends React.Component {
 
+    moveBook = (shelf, book, index) => {
+        console.log(this.props.updateShelfBook)
+        console.log(book, shelf, index)
+        BooksApi.update(book, shelf).then((result) => {
+            book.shelf = shelf;
+            if(this.props.updateShelfBook)
+                this.props.updateShelfBook(index, book);
+            this.props.afterUpdateBook();
+        })
+    }
+
     render() {
-        const { title, books, afterUpdateBook } = this.props;
+        const { title, books } = this.props;
         return (
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{title}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
                         {
-                            books && books.map((book) => (
+                            books && books.map((book, index) => (
                                 <Book key={book.id}
-                                    afterUpdateBook={afterUpdateBook}
+                                    index={index}
+                                    moveBook={this.moveBook}
                                     book={book} />)
                             )
                         }
